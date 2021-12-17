1624e92e5e68cc45b75f050b8295fe4bae4ed05f
Nakinate Buatip <nakinate1710@gmail.com>
2021-12-17 13:28:31 +0700

    completed

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -8,9 +8,16 @@ int main (){
 	ofstream dest;
 	source.open("cheerbook.txt") ;
 	dest.open("cheerbook_copy.txt");
+	string tl;
+	dest <<"-------------------- BOOM ---------------------\n";
+	while (getline(source,tl))
+	{
+		dest<<tl<<"\n";
+	}
+	dest <<"-------------------- HA!! ---------------------";
 	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+
+
 
     source.close();
     dest.close();
